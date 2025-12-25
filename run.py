64f1c3949394aaea838659d9acd6dd26791aef42
TU NOMBRE <TU_EMAIL>
2025-12-25 20:12:41 -0300

    Fix Gunicorn entrypoint using create_app

--- run.py
+++ run.py
@@ -1,10 +1,9 @@
 import os
+from app import app
 
-from app import create_app
-
-app = create_app()
-
-if __name__ == '__main__':
-    debug = str(os.environ.get('APP_DEBUG') or '').strip().lower() in ('1', 'true', 'yes', 'on')
-    use_reloader = str(os.environ.get('APP_RELOAD') or '').strip().lower() in ('1', 'true', 'yes', 'on')
-    app.run(debug=debug, use_reloader=use_reloader)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(
+        host="0.0.0.0",
+        port=port
+    )
